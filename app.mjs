9bf30096ee4b00bd509fea5cf58b78186fff82c1
Ramees12ar <ramees.ar04@gmail.com>
2023-10-30 20:43:11 +0530

    server and packages added

---
+++ app.mjs
@@ -0,0 +1,37 @@
+import express from "express"
+import cors from "cors"
+import config from "config";
+import bodyParser from "body-parser"
+import apiRouter from "./router/routes.mjs";
+import logger from "./logger.mjs";
+
+const app = express()
+app.use(cors({
+    origin: "*",
+    allowedHeaders: "X-Requested-With, authorization, Content-Type, Accept, Cache-Control, DNT, If-Modified-Since, Keep-Alive, Origin, User-Agent",
+    methods: "GET,POST,PUT,DELETE",
+    preflightContinue: false,
+    maxAge: 36000,
+    credentials: true,
+}))
+const { port } = config
+const processApplication = async () => {
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.use("/api", apiRouter)
+    app.use((err, req, res, next) => {
+        if (err) {
+            return res.status(err.status).json({ error: err });
+        }
+        next(); // Pass control to the next middleware if no error occurred
+    });
+    process.on("unhandledRejection", (reason, p) => console.error("Unhandled Rejection at: Promise ", p, reason));
+    app.listen(port, () => {
+        logger.info(`App Started on port :: ${port} ${config.apiUrl}`);
+    });
+};
+
+processApplication().then();
+
+export default app;
